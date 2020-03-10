21065a1218af500534aec5718785a15491ec22e7
Numenization <nicholas.spencer5@gmail.com>
2020-03-10 08:57:54 -0400

    Finished technology categories

--- src/main/websrc/components/admin/technologyCategories.jsx
+++ src/main/websrc/components/admin/technologyCategories.jsx
@@ -5,9 +5,41 @@ import NavBar from "../navbar.jsx";
 import Footer from "../footer.jsx";
 import "../../css/styles.css";
 import "../../css/technologyCategory.css";
+import Technology from "../../data/technologies.js";
 
 class TechnologyCategories extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: [],
+      loading: false,
+      error: null
+    };
+
+    this.getCategories = this.getCategories.bind(this);
+  }
+
+  async getCategories() {
+    this.setState({ loading: true });
+
+    var categories = await Technology.getCategories();
+
+    if (categories.statusText) {
+      this.setState({ error: categories.statusText, loading: false });
+      return;
+    }
+
+    this.setState({ categories: categories, loading: false });
+  }
+
+  async componentDidMount() {
+    this.getCategories();
+  }
+
   render() {
+    const categories = this.state.categories;
+    const removeButtonStyles = {};
+
     return (
       <div className="container">
         <div className="background">
@@ -25,11 +57,41 @@ class TechnologyCategories extends React.Component {
               <h2>New Technology Category</h2>
               <label htmlFor="category-name">Category Name: </label>
               <input id="category-name" type="text"></input>
-              <button id="link-button">Create</button>
+              <button
+                id="link-button"
+                onClick={async e => {
+                  const category = document.getElementById("category-name")
+                    .value;
+                  document.getElementById("category-name").value = "";
+                  await Technology.postCategory(category);
+                  await this.getCategories();
+                }}
+              >
+                Create
+              </button>
             </div>
+
             <div className="category-list">
               <h2>Current Categories</h2>
-              <p>Categories go here</p>
+              {categories.map((category, i) => {
+                return (
+                  <div key={i} id={category.id} className="category-row">
+                    <label>{category.category}</label>
+                    <button
+                      id="link-button"
+                      style={removeButtonStyles}
+                      onClick={async e => {
+                        await Technology.deleteCategory({
+                          id: parseInt(e.target.parentNode.id)
+                        });
+                        await this.getCategories();
+                      }}
+                    >
+                      Remove
+                    </button>
+                  </div>
+                );
+              })}
             </div>
           </div>
         </div>
--- src/main/websrc/data/technologies.js
+++ src/main/websrc/data/technologies.js
@@ -1,4 +1,59 @@
 class Technology {
+  static async getCategories(opts = null) {
+    let url = "/idte/technologyCategories/all";
+    let method = "GET";
+    let res = await Technology.req(method, url, opts);
+    if (res.statusText) {
+      return res;
+    }
+    return res;
+  }
+
+  static async postCategory(opts) {
+    console.log(opts);
+    try {
+      if (!opts) return;
+      let res = await Technology.req(
+        "POST",
+        "/idte/technologyCategories",
+        opts
+      );
+      return res;
+    } catch (e) {
+      console.log(e.request);
+      alert(
+        "Error: " +
+          e.status +
+          "\n" +
+          e.statusText +
+          "\n" +
+          "Make sure all required fields are filled with valid values"
+      );
+    }
+  }
+
+  static async deleteCategory(opts = null) {
+    try {
+      if (!opts) return;
+      let res = await Technology.req(
+        "DELETE",
+        "/idte/technologyCategories",
+        opts
+      );
+      return res;
+    } catch (e) {
+      console.log(e.request);
+      alert(
+        "Error: " +
+          e.status +
+          "\n" +
+          e.statusText +
+          "\n" +
+          "Make sure all required fields are filled with valid values"
+      );
+    }
+  }
+
   static async req(method, url, opts = null) {
     return new Promise(function(resolve, reject) {
       let xhr = new XMLHttpRequest();
